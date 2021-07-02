df3e33ce842ffeee85cdf72a41fd74e39b449965
AnnaYurchenko10 <76647040+AnnaYurchenko10@users.noreply.github.com>
2021-07-03 01:46:49 +0500

    Finish work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,4 @@
 package sample;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,7 +10,6 @@ import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Controller {
@@ -35,53 +33,58 @@ public class Controller {
     private ImageView newImage;
 
     final FileChooser fileChooser = new FileChooser();//выбор файла
-    //private Desktop desktop = Desktop.getDesktop();//объект рабочего стола
 
-    private MyImage image;
-    private Image afterImage;
+    private MyImage image;//объект класса MyImage
+    private Image afterImage;//изображение после обработки
+    private String format;//формат файла
 
     @FXML
     void loadImage(ActionEvent event) {//загрузка изображения
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPG","*.jpg"));//фильтр для файла
         File file = fileChooser.showOpenDialog(loadButton.getScene().getWindow());//выбор файла
+        int i = file.getName().lastIndexOf('.');//индекс точки
+        if (i >= 0) {
+            format = file.getName().substring(i+1);//подстрока после точки (формат)
+        }
         if(file!=null)
         {
-            image = new MyImage(file);
-            originalImage.setImage(image.getImage());
-            processButton.setDisable(false);
+            image = new MyImage(file);//инициализация объекта
+            originalImage.setImage(image.getImage());//вывод изображения в окне
+            processButton.setDisable(false);//активация кнопки "Обработать"
         }
     }
 
     @FXML
-    void process(ActionEvent event) {
+    void process(ActionEvent event) {//обработка изображения
+        long start = System.currentTimeMillis();//начало работы таймера
         if(algorithmComboBox.getValue().equals("Оператор Собеля")) {
-            afterImage = image.operatorSl();
-            newImage.setImage(afterImage);
-            saveButton.setDisable(false);
+            afterImage = image.operatorSl();//обработанное изображение
+            newImage.setImage(afterImage);//вывод обработанного изображения в окно
+            saveButton.setDisable(false);//активация кнопки "Сохранить"
         }
-        else if (algorithmComboBox.getValue().equals("Оператор Лапласа")) {
-            afterImage = image.operatorSl();
-            newImage.setImage(afterImage);
-            saveButton.setDisable(false);
+        else if (algorithmComboBox.getValue().equals("Оператор Робертса")) {
+            afterImage = image.operatorRob();//обработанное изображение
+            newImage.setImage(afterImage);//вывод обработанного изображения в окно
+            saveButton.setDisable(false);//активация кнопки "Сохранить"
         }
         else if (algorithmComboBox.getValue().equals("Оператор Юрченко")) {
-            afterImage = image.operatorYurchenko();
-            newImage.setImage(afterImage);
-            saveButton.setDisable(false);
+            afterImage = image.operatorYurchenko();//обработанное изображение
+            newImage.setImage(afterImage);//вывод обработанного изображения в окно
+            saveButton.setDisable(false);//активация кнопки "Сохранить"
         }
+        long finish = System.currentTimeMillis();//окончание работы таймера
+        long res = finish - start;//затраченное время
+        System.out.println("Прошло времени, мс: " + res);//вывод результата
 
     }
 
 
     @FXML
-    void save(ActionEvent event) {
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPEG","*.jpeg"));//фильтр для файла
-        File file = fileChooser.showSaveDialog(saveButton.getScene().getWindow());
+    void save(ActionEvent event) {//сохранение изображения
+        File file = fileChooser.showSaveDialog(saveButton.getScene().getWindow());//открытие окна для выбора папки сохранения
         if(file!=null)
         {
             try {
-                //ImageIO.write(SwingFXUtils.fromFXImage(newImage.getImage(), null), "jpeg", file);
-                ImageIO.write(image.getNewBufferedImage(),"jpg",file);
+                ImageIO.write(image.getNewBufferedImage(),format,file);//запись изображения в файл
             } catch (IOException e)
             {
                 e.printStackTrace();
@@ -91,9 +94,14 @@ public class Controller {
 
     @FXML
     void initialize() {
+        //фильтр для файла
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPG", "*.jpg"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG",  "*.png"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("BMP", "*.bmp"));
+        //инициализация comboBox
         algorithmComboBox.getItems().add("Оператор Собеля");
-        algorithmComboBox.getItems().add("Оператор Лапласа");
+        algorithmComboBox.getItems().add("Оператор Робертса");
         algorithmComboBox.getItems().add("Оператор Юрченко");
-        algorithmComboBox.getSelectionModel().selectFirst();
+        algorithmComboBox.getSelectionModel().selectFirst();//установка первого текущего элемента
     }
 }
--- src/sample/MyImage.java
+++ src/sample/MyImage.java
@@ -9,16 +9,17 @@ import java.io.File;
 import java.io.FileInputStream;
 
 public class MyImage {
-    private BufferedImage bufferedImage;
-    private BufferedImage newBufferedImage;
-    private Image image;
+    private BufferedImage bufferedImage;//объект для обработки изображения
+    private BufferedImage newBufferedImage;//обработанный объект изображения
+    private Image image;//изображение для вывода в графический интерфейс
 
-    private static int DIFFERENCE = 15;
+    private static int DIFFERENCE = 15;//чувствительность
+
+    private int[][] matrix = new int[3][3];//матрица-маска
 
     public MyImage(File file){//чтение изобрадения из файла
         try {
             image  = new Image(new FileInputStream(file));//инициализация объекта с помощью файлового потока
-            //afterImage = new Image(new FileInputStream(file));
             bufferedImage = ImageIO.read(file);//обрабатываемое изображение
             //получаемое изображение
             newBufferedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), bufferedImage.getType());
@@ -33,24 +34,29 @@ public class MyImage {
         return image;
     }
 
-    public Image operatorSl()
+    public int[][] mask(int [][] matrix, int x, int y)//маска
     {
-        int[][] matrix = new int[3][3];//матрица-маска
+        matrix[0][0] = new Color(bufferedImage.getRGB(x - 1, y - 1)).getGreen();
+        matrix[0][1] = new Color(bufferedImage.getRGB(x - 1, y)).getGreen();
+        matrix[0][2] = new Color(bufferedImage.getRGB(x - 1, y + 1)).getGreen();
+        matrix[1][0] = new Color(bufferedImage.getRGB(x, y - 1)).getGreen();
+        matrix[1][1] = new Color(bufferedImage.getRGB(x, y)).getGreen();
+        matrix[1][2] = new Color(bufferedImage.getRGB(x, y + 1)).getGreen();
+        matrix[2][0] = new Color(bufferedImage.getRGB(x + 1, y - 1)).getGreen();
+        matrix[2][1] = new Color(bufferedImage.getRGB(x + 1, y)).getGreen();
+        matrix[2][2] = new Color(bufferedImage.getRGB(x + 1, y + 1)).getGreen();
 
+        return matrix;
+    }
+
+
+    public Image operatorSl()//оператор Собеля
+    {
         //алгоритм получения оттенков цвета
         for(int y = 1; y < bufferedImage.getHeight() - 1; y++) {
             for (int x = 1; x < bufferedImage.getWidth() - 1; x++) {
-                matrix[0][0] = new Color(bufferedImage.getRGB(x - 1, y - 1)).getGreen();
-                matrix[0][1] = new Color(bufferedImage.getRGB(x - 1, y)).getGreen();
-                matrix[0][2] = new Color(bufferedImage.getRGB(x - 1, y + 1)).getGreen();
-                matrix[1][0] = new Color(bufferedImage.getRGB(x, y - 1)).getGreen();
-                matrix[1][1] = new Color(bufferedImage.getRGB(x, y)).getGreen();
-                matrix[1][2] = new Color(bufferedImage.getRGB(x, y + 1)).getGreen();
-                matrix[2][0] = new Color(bufferedImage.getRGB(x + 1, y - 1)).getGreen();
-                matrix[2][1] = new Color(bufferedImage.getRGB(x + 1, y)).getGreen();
-                matrix[2][2] = new Color(bufferedImage.getRGB(x + 1, y + 1)).getGreen();
-
-                int border = (int) convolution(matrix);
+                mask(matrix,x,y);
+                int border = (int) convolutionSl(matrix);
                 newBufferedImage.setRGB(x, y, ~(border | border << 8 | border << 16));
             }
         }
@@ -58,22 +64,41 @@ public class MyImage {
         return SwingFXUtils.toFXImage(newBufferedImage, null );
     }
 
-    public double convolution(int[][] matrix) {
+    public double convolutionSl(int[][] matrix) {//вычисление градиента
         int gx = ((matrix[2][0] + 2 * matrix[2][1] + matrix[2][2]) - (matrix[0][0] + 2 * matrix[0][1] + matrix[0][2]));
         int gy = ((matrix[0][2] + 2 * matrix[1][2] + matrix[2][2]) - (matrix[0][0] + 2 * matrix[1][0] + matrix[2][0]));
         return Math.sqrt(Math.pow(gy, 2) + Math.pow(gx, 2));
     }
 
-    public Image operatorYurchenko()
+    public Image operatorRob()//Оператор Робертса
     {
-        System.out.println(bufferedImage.getHeight());
-        System.out.println(bufferedImage.getWidth());
-        for(int y = 1; y < bufferedImage.getHeight()-3; y++) {
-            for (int x = 1; x < bufferedImage.getWidth()-3; x++) {
-                if (Math.abs(new Color(bufferedImage.getRGB(x,y)).getRed() - new Color(bufferedImage.getRGB(x+3,y+3)).getRed())>DIFFERENCE ||
-                        Math.abs(new Color(bufferedImage.getRGB(x,y)).getGreen() - new Color(bufferedImage.getRGB(x+3,y+3)).getGreen())>DIFFERENCE ||
-                        Math.abs(new Color(bufferedImage.getRGB(x,y)).getBlue() - new Color(bufferedImage.getRGB(x+3,y+3)).getBlue())>DIFFERENCE) {
-                    newBufferedImage.setRGB(x,y,Color.BLACK.getRGB());
+        //алгоритм получения оттенков цвета
+        for(int y = 1; y < bufferedImage.getHeight() - 1; y++) {
+            for (int x = 1; x < bufferedImage.getWidth() - 1; x++) {
+                mask(matrix,x,y);
+                int border = (int) convolutionPb(matrix);
+                newBufferedImage.setRGB(x, y, ~(border | border << 8 | border << 16));
+            }
+        }
+        //приведение обработанного изображения к классу Image для вывода в оконном интерфейсе
+        return SwingFXUtils.toFXImage(newBufferedImage, null );
+    }
+
+    public double convolutionPb(int[][] matrix) {//вычисление градиента
+        int gx = matrix[2][2] - matrix [1][1];
+        int gy = matrix[2][1] - matrix [1][2];
+        return Math.sqrt(Math.pow(gy, 2) + Math.pow(gx, 2));
+    }
+
+    public Image operatorYurchenko()//собственный метод для выделения контуров на изображении
+    {
+        for(int y = 0; y < bufferedImage.getHeight()-2; y++) {
+            for (int x = 0; x < bufferedImage.getWidth()-2; x++) {
+                //проверка разности значения цвета пикселей
+                if (Math.abs(new Color(bufferedImage.getRGB(x,y)).getRed() - new Color(bufferedImage.getRGB(x+2,y+2)).getRed())>DIFFERENCE ||
+                        Math.abs(new Color(bufferedImage.getRGB(x,y)).getGreen() - new Color(bufferedImage.getRGB(x+2,y+2)).getGreen())>DIFFERENCE ||
+                        Math.abs(new Color(bufferedImage.getRGB(x,y)).getBlue() - new Color(bufferedImage.getRGB(x+2,y+2)).getBlue())>DIFFERENCE) {
+                    newBufferedImage.setRGB(x,y,Color.BLACK.getRGB());//запись пикселя
                 }
                 else{
                     newBufferedImage.setRGB(x,y, Color.WHITE.getRGB());
@@ -81,6 +106,7 @@ public class MyImage {
             }
         }
 
+        //приведение обработанного изображения к классу Image для вывода в оконном интерфейсе
         return SwingFXUtils.toFXImage(newBufferedImage, null);
     }
 
